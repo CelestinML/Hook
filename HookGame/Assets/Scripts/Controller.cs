c77aac7f18a15eba0aa622964fad7fc2a4082521
Célestin Moënne-Loccoz <celestin.moenne@gmail.com>
2020-09-11 19:20:33 +0200

    Clean de l'organisation et du script de contrôle
    trouver un moyen de ne pas utiliser j_decreasing et privilégier gravity et régler le problème de "flottement" un peu au dessus du sol parfois

--- HookGame/Assets/Scripts/Controller.cs
+++ HookGame/Assets/Scripts/Controller.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    private Animator anim;
-
     public GameObject eyes;
+    public GameObject hook;
 
     public Boolean air_control = true;
     public float gravity = 1;
@@ -19,20 +19,19 @@ public class Controller : MonoBehaviour
     public float h_slow_smoothening = 1;
     public float h_slow_smoothening_in_air = 0.5f;
     public float j_force = 80;
-    public float det_distance = 0.001f;
+    public float det_distance = 0.0001f;
     public float j_decreasing;
     public float strong_j_decreasing;
 
     private float current_h_speed;
-    public float current_v_speed;
-    //public Boolean in_air = false;
+    private float current_v_speed;
 
-    //private GameObject player;
     private float h_dimension, v_dimension;
 
-    public Boolean j_request;
-    private Boolean was_jumping;
+    private Boolean j_request;
     private float movement_request;
+    private Boolean hook_request;
+
     private float x_speed, y_speed;
     private Vector3 speed;
 
@@ -42,23 +41,26 @@ public class Controller : MonoBehaviour
     private RaycastHit2D foot_stuck_right, foot_stuck_left, knee_stuck_right, knee_stuck_left;
     private RaycastHit2D head_stuck_right, head_stuck_left;
     private RaycastHit2D ceiling_left, ceiling_right, ceiling_middle;
-    //For walljump
+    //For walljump (ToDo)
     private RaycastHit2D top_right_wall, bottom_right_wall, top_left_wall, bottom_left_wall;
 
     //Booleans
-    public Boolean landed;
-    public Boolean grounded;
-    public Boolean wall_stuck_right, wall_stuck_left;
-    public Boolean head_stuck;
+    private Boolean landed;
+    private Boolean grounded;
+    private Boolean wall_stuck_right, wall_stuck_left;
+    private Boolean head_stuck;
 
+    //Pour tracker les yeux, et viser avec le grappin
     private Vector3 mouse_pos;
+
+    //Pour gérer les animations
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
 
-        was_jumping = false;
         j_decreasing = j_force / 200;
         strong_j_decreasing = j_decreasing * 4;
 
@@ -140,21 +142,18 @@ public class Controller : MonoBehaviour
         // \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/  \/ //
 
         /////////////////////////////Vitesse verticale//////////////////////////////////
-        if (!was_jumping && j_request && landed)
-        {
+        if (j_request && landed)
+            {
             //On est au sol et on demande de sauter
             //La force de saut est donc au maximum
             y_speed = j_force;
-            //On enregistre le fait qu'on est en train de sauter
-            was_jumping = true;
         }
         else if (current_v_speed > (j_force / 10))
         {
-            if (was_jumping)
+            if (j_request)
             {
                 //Si le saut n'a pas été déclenché mais qu'on veut encore sauter en maintenant espace
                 //On monte encore mais de moins en moins vite jusqu'à y_speed = 0
-                //y_speed = current_v_speed - j_decreasing;
                 y_speed = Mathf.Lerp(current_v_speed, 0, j_decreasing);
             }
             else
@@ -165,8 +164,7 @@ public class Controller : MonoBehaviour
         }
         else
         {
-            //On maintient une vitesse de chute maximale pour éviter des déplacements insensés, et des bugs
-            //y_speed = Math.Max(current_v_speed - gravity, -max_falling_speed);
+            //On maintient une vitesse de chute maximale pour éviter des chutes insensées, et des bugs
             y_speed = -Mathf.Lerp(Math.Abs(current_v_speed), max_falling_speed, gravity);
         }
         //On enregistre toujours la dernière vitesse horizontale
@@ -229,6 +227,7 @@ public class Controller : MonoBehaviour
         speed = new Vector3(x_speed, y_speed, 0);
         transform.position += speed;
 
+        //Positionnement des yeux
         eyes.transform.position = transform.position + mouse_pos.normalized * 0.16f;
     }
 
@@ -243,7 +242,6 @@ public class Controller : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))
         {
             j_request = false;
-            was_jumping = false;
         }
         movement_request = Input.GetAxis("Horizontal");
         if (movement_request != 0)
@@ -253,6 +251,14 @@ public class Controller : MonoBehaviour
         else
         {
             anim.SetBool("Moving", false);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            hook_request = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            hook_request = false;
         }
 
         //Calcul de la position de la souris par rapport au joueur
